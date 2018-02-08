77081a24db3e1645de2c84ff0a00a3adcb9b4b7c
X1-CARBON\David <dfava80@gmail.com>
2018-02-08 20:46:13 +0100

    First step to lift the state

--- src/index.js
+++ src/index.js
@@ -8,65 +8,19 @@ import {
     Route,
     Switch,
     Redirect
-  } from 'react-router-dom'
+} from 'react-router-dom'
 
 import registerServiceWorker from './registerServiceWorker';
-import { 
-    ApplicationHomePage, 
-    ApplicationVaultPage, 
-    ApplicationDragoPage, 
-    ApplicationConfigPage,
-    ApplicationGabcoinPage,
-    // ApplicationExchangePage,
-    Whoops404 } from './App';
-
+import App from './App';
 import './index.module.css';
 
-var appHashPath = true;
-
-
-
-// Detectiong if the app is running inside Parity client
-var pathArray = window.location.hash.split( '/' );
-// console.log(pathArray[2]);
-if (typeof window.parity !== 'undefined') {
-
-    // Need to check if this works inside the Parity UI
-    // appHashPath = pathArray[2];
-    appHashPath = 'web';
-    } else {
-    appHashPath = 'web';
-    }
-// console.log(appHashPath);
-// console.log(location);
-
-// Setting the routes. 
-// Component Whoops404 is loaded if a page does not exist.
-
 ReactDOM.render(
-    <HashRouter>
-        <Switch>
-          <Route exact path={ "/app/" + appHashPath + "/home" } component={ApplicationHomePage} />
-          <Route path={ "/app/" + appHashPath + "/vault" } component={ApplicationGabcoinPage} />
-          <Route path={ "/app/" + appHashPath + "/vaultv2" } component={ApplicationVaultPage} />
-          {/* <Route exact path={ "/app/" + appHashPath + "/drago/dashboard" } component={ApplicationDragoPage} /> */}
-          {/* <Route exact path={ "/app/" + appHashPath + "/drago/funds" } component={ApplicationDragoPage} /> */}
-          <Route path={ "/app/" + appHashPath + "/drago" } component={ApplicationDragoPage} />
-          <Route path={ "/app/" + appHashPath + "/config" } component={ApplicationConfigPage} />
-          {/* <Route path={ "/app/" + appHashPath + "/exchange" } component={ApplicationExchangePage} /> */}
-          {/* <Redirect from="/exchange" to={ "/app/" + appHashPath + "/exchange" } />  */}
-          <Redirect from="/vault/" to={ "/app/" + appHashPath + "/vault" } />
-          <Redirect from="/vaultv2/" to={ "/app/" + appHashPath + "/vaultv2" } />
-          <Redirect from="/drago" to={ "/app/" + appHashPath + "/drago" } />
-          <Redirect from="/" to={ "/app/" + appHashPath + "/home" } />
-          <Route component={Whoops404} />
-        </Switch>
-    </HashRouter>,
+    <App />,
     document.getElementById('root'))
 
 registerServiceWorker()
 
 // Hot Module Reload
-// if (module.hot) {
-//     module.hot.accept();
-// }
+if (module.hot) {
+    module.hot.accept();
+}
