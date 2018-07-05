9687913e003b7e596af75ab41cc61f2b2705a3d9
David Fava <dfava80@gmail.com>
2018-07-05 16:34:02 +0100

    test

--- scripts/deploy-webapp-docker.sh
+++ scripts/deploy-webapp-docker.sh
@@ -4,8 +4,8 @@
 # echo $NUMBER
 docker build --no-cache -t webapp-v1 -f scripts/containers/beta-dev/Dockerfile .
 docker login https://rb-registry.endpoint.network/ -u wnz99 -p $JENKINS_PASSWORD
-docker tag webapp-v1 rb-registry.endpoint.network:webapp-v1-$NUMBER
-docker push rb-registry.endpoint.network/webapp-v1-$NUMBER
+docker tag webapp-v1 rb-registry.endpoint.network/webapp-v1:$NUMBER
+docker push rb-registry.endpoint.network/webapp-v1:$NUMBER
 # printenv
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -13,5 +13,5 @@ mv ./kubectl /usr/local/bin/kubectl
 wget https://github.com/rancher/cli/releases/download/v2.0.3-rc1/rancher-linux-amd64-v2.0.3-rc1.tar.gz -O rancher-linux-amd64-v2.0.3-rc1.tar.gz
 tar xzvf rancher-linux-amd64-v2.0.3-rc1.tar.gz && cd rancher-v2.0.3-rc1/ && chmod +x rancher
 echo "1" | ./rancher login https://dev-03.endpoint.network --token $RANCHER_TOKEN
-./rancher kubectl set image deployment/webapp-v1-staging webapp-v1-staging=rb-registry.endpoint.network/webapp-v1-$NUMBER
+./rancher kubectl set image deployment/webapp-v1-staging webapp-v1-staging=rb-registry.endpoint.network/webapp-v1:$NUMBER
 
