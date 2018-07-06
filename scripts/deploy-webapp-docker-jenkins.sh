40fe864ec283e195a8257531f719219ae39094c3
David Fava <dfava80@gmail.com>
2018-07-06 13:26:50 +0100

    test

--- scripts/deploy-webapp-docker-jenkins.sh
+++ scripts/deploy-webapp-docker-jenkins.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# printenv
 NUMBER=$(git log --pretty=oneline | wc -l)
 DATE=$(date +%Y%m%d)
 TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 HASH=$(git rev-parse --short HEAD)
 BRANCH=${GIT_BRANCH#*/}
-# echo $NUMBER
 docker build --no-cache -t webapp-v1 -f scripts/containers/beta-dev/Dockerfile .
 docker login https://rb-registry.endpoint.network/ -u wnz99 -p $JENKINS_PASSWORD
 docker tag webapp-v1 rb-registry.endpoint.network/webapp-v1:latest
 docker tag webapp-v1 rb-registry.endpoint.network/webapp-v1:$TAG-$BRANCH-$HASH-$DATE-$NUMBER
 docker push rb-registry.endpoint.network/webapp-v1
-# printenv
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
--- src/_utils/version.js
+++ src/_utils/version.js
@@ -1,1 +1,1 @@
-export const APP_VERSION = 'v0.3.0-alpha-testing-de2c5ea-20180706-269'
+export const APP_VERSION = 'v0.3.0-alpha-testing-95049b8-20180706-270'
