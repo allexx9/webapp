7bb2b11b2461c186fc8042430d5eabe8e8fd958d
David Fava <dfava80@gmail.com>
2018-07-06 10:14:58 +0100

    test

--- scripts/deploy-webapp-docker.sh
+++ scripts/deploy-webapp-docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # printenv
-NUMBER=$(cat BUILD_NUMBER) && export NUMBER
+# NUMBER=$(cat BUILD_NUMBER) && export NUMBER
 DATE=$(date +%Y%m%d)
+TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+HASH=$(git rev-parse --short HEAD)
+BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 # echo $NUMBER
 docker build --no-cache -t webapp-v1 -f scripts/containers/beta-dev/Dockerfile .
 docker login https://rb-registry.endpoint.network/ -u wnz99 -p $JENKINS_PASSWORD
-docker tag webapp-v1 rb-registry.endpoint.network/webapp-v1:$DATE-$NUMBER
-docker push rb-registry.endpoint.network/webapp-v1:$DATE-$NUMBER
+docker tag webapp-v1 rb-registry.endpoint.network/webapp-v1:$TAG-$BRANCH-$HASH-$DATE-$NUMBER
+docker push rb-registry.endpoint.network/webapp-v1:$TAG-$BRANCH-$HASH-$DATE-$NUMBER
 # printenv
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
@@ -14,5 +17,4 @@ mv ./kubectl /usr/local/bin/kubectl
 wget https://github.com/rancher/cli/releases/download/v2.0.3-rc1/rancher-linux-amd64-v2.0.3-rc1.tar.gz -O rancher-linux-amd64-v2.0.3-rc1.tar.gz
 tar xzvf rancher-linux-amd64-v2.0.3-rc1.tar.gz && cd rancher-v2.0.3-rc1/ && chmod +x rancher
 echo "1" | ./rancher login https://dev-03.endpoint.network --token $RANCHER_TOKEN
-./rancher kubectl set image deployment/webapp-v1-staging webapp-v1-staging=rb-registry.endpoint.network/webapp-v1:$DATE-$NUMBER
-
+./rancher kubectl set image deployment/webapp-v1-staging webapp-v1-staging=rb-registry.endpoint.network/webapp-v1:$TAG-$BRANCH-$HASH-$DATE-$NUMBER
---
+++ src/_utils/version.js
@@ -0,0 +1,1 @@
+export const APP_VERSION = 'v0.3.0-alpha-testing-5a3d42b-20180706-254'
