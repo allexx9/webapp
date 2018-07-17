dc3bc86e0e35c2d48ff7a679b1d1ab9e96024068
X1-CARBON\David <dfava80@gmail.com>
2018-07-17 14:00:31 +0200

    Copyright amended

--- src/Elements/elementBottomStatusBar.js
+++ src/Elements/elementBottomStatusBar.js
@@ -143,7 +143,7 @@ export default class ElementBottomStatusBar extends Component {
     return (
       <Row className={styles.networkStatus} between="xs">
         <Col xs={6} className={styles.copyright}>
-          ©2017 RigoInvestment. All rights reserved. {APP_VERSION}
+          ©2018 Rigo Investment Sagl. All rights reserved. {APP_VERSION}
         </Col>
         <Col xs={6} className={styles.networkStatusCounter}>
           <div className={styles.networkDataContainer}>
