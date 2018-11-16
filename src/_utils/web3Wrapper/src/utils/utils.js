e7e95a5465c5852c47f6806dea4f5bf6e90b65a5
Federico <federico.obialero@gmail.com>
2018-11-16 12:33:56 +0100

    refactored blockChunks function

--- src/_utils/web3Wrapper/src/utils/utils.js
+++ src/_utils/web3Wrapper/src/utils/utils.js
@@ -8,42 +8,21 @@ export const errorMsg = error => {
   return error.charAt(0).toUpperCase() + error.slice(1)
 }
 
-export const blockChunks = (start, end, chunk) => {
-  let rangesArray = []
-  let i = 0
-  let fromBlock = end - chunk
-  let toBlock = end
-
-  if (end - chunk < start) {
-    rangesArray.push({
-      fromBlock: start,
-      toBlock: 'latest'
-    })
-    return rangesArray
-  }
-  while (toBlock > start) {
-    if (i === 0) {
-      rangesArray.push({
-        fromBlock: fromBlock + 1,
-        toBlock: 'latest'
-      })
-    } else {
-      rangesArray.push({
-        fromBlock: fromBlock + 1,
-        toBlock: toBlock
-      })
+export const blockChunks = async (start, end, chunkSize, web3) => {
+  const startBlock = start
+  const chunks = []
+  let endBlock = end
+  if (endBlock === 'latest') {
+    try {
+      endBlock = await web3.eth.getBlockNumber()
+    } catch (e) {
+      return console.error(e)
     }
-    i++
-    if (i > 100) break
-    if (fromBlock - chunk < start) {
-      rangesArray.push({
-        fromBlock: Number(start),
-        toBlock: fromBlock
-      })
-      break
-    }
-    fromBlock = fromBlock - chunk
-    toBlock = toBlock - chunk
   }
-  return rangesArray
+  for (let i = startBlock - 1; i < endBlock; i += chunkSize) {
+    const fromBlock = i + 1
+    const toBlock = i + chunkSize > endBlock ? end : i + chunkSize
+    chunks.push([fromBlock, toBlock])
+  }
+  return chunks
 }
