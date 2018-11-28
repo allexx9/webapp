1ef0934241ce0f61096e3bb43ed11e6e9f0ea8ba
David Fava <dfava80@gmail.com>
2018-11-28 21:35:38 +0000

    Minor fix to endpoints url

--- src/_utils/const.js
+++ src/_utils/const.js
@@ -118,7 +118,7 @@ export const EP_RIGOBLOCK_RP_PROD_WS = 'wss://ropsten.dev.endpoint.network/ws'
 export const EP_RIGOBLOCK_MN_DEV = 'https://mainnet.dev.endpoint.network/rpc'
 export const EP_RIGOBLOCK_MN_DEV_WS = 'wss://mainnet.dev.endpoint.network/ws'
 export const EP_RIGOBLOCK_MN_PROD = 'https://mainnet.dev.endpoint.network/rpc'
-export const EP_RIGOBLOCK_MN_PROD_WS = 'wss://ropsten.dev.endpoint.network/ws'
+export const EP_RIGOBLOCK_MN_PROD_WS = 'wss://mainnet.dev.endpoint.network/ws'
 
 // Allowed endpoints in config section
 export const INFURA = 'infura'
@@ -202,7 +202,7 @@ export const ENDPOINTS = {
     https: {
       kovan: {
         dev: EP_RIGOBLOCK_KV_DEV,
-        prod: EP_RIGOBLOCK_RP_PROD
+        prod: EP_RIGOBLOCK_KV_PROD
       },
       ropsten: {
         dev: EP_RIGOBLOCK_RP_DEV,
