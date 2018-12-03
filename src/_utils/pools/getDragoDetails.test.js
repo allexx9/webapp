11aea50868b2bf8272822c909810928692294fa9
David Fava <dfava80@gmail.com>
2018-12-03 08:50:21 +0000

    Some fixes to package.json

--- src/_utils/pools/getDragoDetails.test.js
+++ src/_utils/pools/getDragoDetails.test.js
@@ -1,9 +1,13 @@
 const contractName = 'Drago'
 
+// Test
+//
+
 describeContract(contractName, () => {
   describe('getDragoDetails test', () => {
     it('Correctly getDragoDetails test', async () => {
       const ownerAddress = true
+      console.log('ok')
       expect(ownerAddress).toBe(true)
     })
   })
